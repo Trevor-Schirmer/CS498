b2609c6b08f19d562adc5eaf7f7ba981a468e3ca
Adam Brassfield <anbrassfield@gmail.com>
2018-04-13 12:36:14 -0400

    Updated webpage

--- results.php
+++ results.php
@@ -4,8 +4,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Results</title>
   <style>
+  .build-banner {
+  }
+
+  .test-results {
+  }
+
+  .error-results {
+  }
   </style>
 </head>
 <body>
+  <?php ?>
+  <div class="build-banner">
+    <?php ?>
+  </div>
+  <div class="test-results">
+    <?php ?>
+  </div>
+  <div class="error-results">
+    <?php ?>
+  </div>
 </body>
 </html>
