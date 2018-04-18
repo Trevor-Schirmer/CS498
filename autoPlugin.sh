1242b6e69987989c9adbd5f012e82cb1609a0106
TrevorSchirmer <TrevorSchirmer@users.noreply.github.com>
2018-04-18 11:15:58 -0400

    Fresh Dev Start

--- autoPlugin.sh
+++
@@ -1,2 +0,0 @@
-mvn install
-java -jar jenkins-cli.jar -s http://128.163.143.152:8080/ -"remoting" install-plugin /home/anbr243/.jenkins/workspace/helloWorld/target/demo.hpi --usernam='tpsc223' --password='password'
