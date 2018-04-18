ffc09d1322d761c616c881afc01c8cfb464f7ac8
TrevorSchirmer <TrevorSchirmer@users.noreply.github.com>
2018-04-18 10:38:28 -0400

    Re-added dependencies

--- src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -8,8 +8,8 @@ import hudson.model.AbstractProject;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.Builder;
-//import hudson.tasks.Publisher;
-//import hudson.tasks.Recorder;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 
